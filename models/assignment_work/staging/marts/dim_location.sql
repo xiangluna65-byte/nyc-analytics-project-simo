8e77a6e34dde05f2680237bf8c317ed15bcfb70e
Simo Xiang <xiangluna65@gmail.com>
2026-02-22 23:49:09 +0000

    dim_location created

---
+++ models/assignment_work/staging/marts/dim_location.sql
@@ -0,0 +1,25 @@
+-- Location dimension shared by both restaurant applications and 311 service reqs
+
+WITH all_locations AS (
+   -- Get locations from 311 requests
+   SELECT DISTINCT borough, zip_code
+   FROM {{ ref('stg_311_service_requests_dot') }}
+   WHERE borough IS NOT NULL
+
+   UNION DISTINCT
+
+   -- Get locations from restaurant applications
+   SELECT DISTINCT borough, zip_code
+   FROM {{ ref('stg_nyc_open_restaurant_apps') }}
+   WHERE borough IS NOT NULL
+),
+
+location_dimension AS (
+   SELECT
+       {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS location_key,
+       borough,
+       zip_code
+   FROM all_locations
+)
+
+SELECT * FROM location_dimension
