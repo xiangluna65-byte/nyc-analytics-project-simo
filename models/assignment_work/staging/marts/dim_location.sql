2cf60dd6b3a838ebab183d037e081971ab37b893
Simo Xiang <xiangluna65@gmail.com>
2026-02-23 00:24:26 +0000

    added time_of_submission to resturant table

--- models/assignment_work/staging/marts/dim_location.sql
+++ models/assignment_work/staging/marts/dim_location.sql
@@ -2,24 +2,29 @@
 
 WITH all_locations AS (
    -- Get locations from 311 requests
-   SELECT DISTINCT borough, incident_zip
+   SELECT DISTINCT 
+        borough, 
+        incident_zip AS zip_code
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
 
    UNION DISTINCT
 
    -- Get locations from restaurant applications
-   SELECT DISTINCT borough, incident_zip
+   SELECT DISTINCT 
+          borough, 
+          incident_zip AS zip_code
+       -- TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
 ),
 
 location_dimension AS (
    SELECT
-       {{ dbt_utils.generate_surrogate_key(['borough', 'incident_zip']) }} AS location_key,
+       {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS location_key,
        borough,
        incident_zip
    FROM all_locations
 )
 
-SELECT * FROM location_dimension
+SELECT * FROM location_dimension --TODO replace ??s with what to select. HINT: May be quite simple!
--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -1,10 +1,11 @@
 SELECT
    borough,
+   time_of_submission,
    LENGTH(CAST(incident_zip AS STRING)) AS zip_length,
    incident_zip,
    COUNT(*) AS count
 FROM `simoxiang-cis-4400-s26.nyc_311_raw_data.source_dot_service_requests_history`
 WHERE incident_zip IS NOT NULL
   AND LENGTH(CAST(incident_zip AS STRING)) != 5
-GROUP BY borough, zip_length, incident_zip
+GROUP BY borough, time_of_submission, zip_length, incident_zip
 ORDER BY count DESC
