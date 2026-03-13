daaa0331a7b1ba8ce8b25088c7b04d4bb6808b82
Simo Xiang <xiangluna65@gmail.com>
2026-03-13 20:11:43 +0000

    rewrite fact tables

--- models/assignment_work2/marts/fact_complaints.sql
+++ models/assignment_work2/marts/fact_complaints.sql
@@ -1,7 +1,51 @@
-SELECT
-    unique_key AS complaint_id,
-    complaint_type,
-    {{ dbt_utils.generate_surrogate_key(['CAST(created_date AS DATE)']) }} AS date_key,
-    {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS location_key,
-    1 AS complaint_count
-FROM {{ ref('stg_311service_requests') }}
+-- Fact table for 311 complaints
+-- Grain: one row per complaint
+
+WITH complaints AS (
+    SELECT *
+    FROM {{ ref('stg_311service_requests') }}
+),
+
+dim_date AS (
+    SELECT
+        date_key, 
+        full_date
+    FROM {{ ref('dim_date') }}
+),
+
+dim_location AS (
+    SELECT
+        location_key,
+        borough,
+        zip_code
+    FROM {{ ref('dim_location') }}
+),
+
+final AS (
+    SELECT
+        -- Natural key
+        c.unique_key AS complaint_id,
+
+        -- Complaint attributes
+        c.complaint_type,
+
+        -- Foreign keys
+        d.date_key,
+        l.location_key,
+
+        -- Measure
+        1 AS complaint_count
+
+    FROM complaints c
+
+    -- Date join
+    LEFT JOIN dim_date d
+        ON CAST(c.created_date AS DATE) = d.full_date
+
+    -- Location join
+    LEFT JOIN dim_location l
+        ON c.borough = l.borough
+        AND c.zip_code = l.zip_code
+)
+
+SELECT * FROM final
--- models/assignment_work2/marts/fact_permits.sql
+++ models/assignment_work2/marts/fact_permits.sql
@@ -1,7 +1,49 @@
-SELECT
-    permit_id,
-    work_type,
-    permit_type,
-    {{ dbt_utils.generate_surrogate_key(['CAST(job_start_date AS DATE)']) }} AS date_key,
-    {{ dbt_utils.generate_surrogate_key(['borough', 'zip_code']) }} AS location_key,
-FROM {{ ref('stg_permit_issuance') }}
+-- Fact table for permit issuance
+-- Grain: one row per permit
+
+WITH permits AS (
+    SELECT *
+    FROM {{ ref('stg_permit_issuance') }}
+),
+
+dim_date AS (
+    SELECT 
+        date_key, 
+        full_date
+    FROM {{ ref('dim_date') }}
+),
+
+dim_location AS (
+    SELECT 
+        location_key, 
+        borough, 
+        zip_code
+    FROM {{ ref('dim_location') }}
+),
+
+final AS (
+    SELECT
+        -- Natural key
+        p.permit_id,
+
+        -- Permit attributes
+        p.work_type,
+        p.permit_type,
+
+        -- Foreign keys
+        d.date_key,
+        l.location_key
+
+    FROM permits p
+
+    -- Date join
+    LEFT JOIN dim_date d
+        ON CAST(p.job_start_date AS DATE) = d.full_date
+
+    -- Location join
+    LEFT JOIN dim_location l
+        ON p.borough = l.borough
+        AND p.zip_code = l.zip_code
+)
+
+SELECT * FROM final
